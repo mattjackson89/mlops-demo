a660db26f248edee142261b1fc0f68ee610fbd32
mjackson89 <matthew.jackson@netacea.com>
2021-09-18 19:30:55 +0100

    Updates notebook to train v1

--- update-models.sh
+++ update-models.sh
@@ -1,3 +1,18 @@
+#!/usr/bin/env bash
+
 # This is a very simple way to achieve this, in reality this would be done using the model registry
+
+#!/usr/bin/env bash
+
+VERSION=${1:-v1}
+
+# Copy the models 
 cp -r data-science/models demo-api/app/
-docker-compose up -d --no-deps --build demo-api # Can this be improved using a mounted directory??
+
+# Promote -v to latest 
+rm -rf demo-api/app/models/latest
+echo demo-api/app/models/"$VERSION"
+cp -r demo-api/app/models/"$VERSION" demo-api/app/models/latest
+
+# docker-compose up -d --no-deps --build demo-api # Can this be improved using a mounted directory??
+# Select the latest model version
